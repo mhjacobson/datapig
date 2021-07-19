8a2ca700ed81ab455350dfffc53a4977ff27104d
Matt Jacobson <mhjacobson@me.com>
2021-07-19 02:03:26 -0400

    datapig.pl: add some "features"

--- datapig.pl
+++ datapig.pl
@@ -5,12 +5,19 @@
 # author: Matt Jacobson
 
 use strict;
+use Getopt::Std;
+
+# -x -- show text symbols, not data/bss
+# -t -- show the total size of listed symbols
+my %opts = ();
+getopts("xt", \%opts);
 
 my %addrs = ();
 my %types = ();
 my $last_addr = 0;
 my $last_name = "???";
 my %sizes = ();
+my $total_size = 0;
 
 while (<>) {
 	if (/^([0-9A-Fa-f]+) ([A-Za-z]) (.*)$/) {
@@ -39,7 +46,15 @@ foreach my $name (sort { $addrs{$a} <=> $addrs{$b} } keys %addrs) {
 foreach my $name (sort { $sizes{$b} <=> $sizes{$a} } keys %sizes) {
 	my $type = $types{$name};
 
-	if ($type eq "d" || $type eq "D" || $type eq "b" || $type eq "B") {
-		printf("%8x %s\n", $sizes{$name}, $name);
+	if ((!$opts{x} && ($type eq "d" || $type eq "D" || $type eq "b" || $type eq "B")) ||
+	     ($opts{x} && ($type eq "t" || $type eq "T")))
+	{
+		my $size = $sizes{$name};
+		printf("%8x %s\n", $size, $name);
+		$total_size += $size;
 	}
+}
+
+if ($opts{t}) {
+	printf("\n%8x TOTAL\n", $total_size);
 }
